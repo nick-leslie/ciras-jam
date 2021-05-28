6cc795d4247673236e1ec8eeb0f6c5a65879a2a8
guy living <nick.leslie303@gmail.com>
2021-05-28 09:39:47 -0400

    started working on AI detection

--- ciras game jam/Assets/scripts/AI/AIBrain.cs
+++ ciras game jam/Assets/scripts/AI/AIBrain.cs
@@ -15,12 +15,12 @@ public class AIBrain : MonoBehaviour
     }
     [Header("flags")]
     [Header("pathfiding type")]
-    private bool patrol;
-    private bool flying;
+    public bool patrol;
+    public bool flying;
     [Header("combat type")]
-    private bool ranged;
-    private bool collison;
-    private bool melee;
+    public bool ranged;
+    public bool collison;
+    public bool melee;
     public bool hold;
     // Start is called before the first frame update
     void Start()
--- ciras game jam/Assets/scripts/AI/AIDetection.cs
+++ ciras game jam/Assets/scripts/AI/AIDetection.cs
@@ -4,27 +4,67 @@ using UnityEngine;
 
 public class AIDetection : MonoBehaviour
 {
+    [SerializeField]
     private AIBrain brain;
+    [SerializeField]
+    private List<GameObject> collisons;
+    [SerializeField]
+    private string[] targetTags;
     // Start is called before the first frame update
     void Start()
     {
         //this script grabs the brain from the parrent 
-       brain = transform.parent.GetComponent<AIBrain>();
+        if (brain == null)
+        {
+            brain = transform.parent.GetComponent<AIBrain>();
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
-    }
-    private void OnCollisionStay2D(Collision2D collision)
-    {
-        //send out a ray cast to the player and see if it collides while player is in the detection zone
-        Vector3 playerDirection = brain.Player.transform.position - transform.position;
-        RaycastHit2D lineOfSight = Physics2D.Raycast(transform.position, playerDirection);
-        if(lineOfSight.collider != null)
+        if (collisons.Count > 0)
         {
-            Debug.Log(lineOfSight.collider.name);
+            for (int i = 0; i < collisons.Count; i++)
+            {
+                Debug.Log("beeing called");
+                //send out a ray cast to the player and see if it collides while player is in the detection zone
+                Vector3 targetDirection = collisons[i].transform.position - transform.position;
+                RaycastHit2D lineOfSight = Physics2D.Raycast(transform.position, targetDirection);
+                Debug.DrawLine(transform.position, lineOfSight.collider.transform.position);
+                if (lineOfSight.collider != null)
+                {
+                    if(lineOfSight.collider.gameObject == collisons[i].gameObject)
+                    {
+                        detected();
+                    }
+                }
+            }
+        }
+    }
+    void detected()
+    {
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        for (int i = 0; i < targetTags.Length; i++)
+        {
+            if(collision.gameObject.CompareTag(targetTags[i]))
+            {
+                collisons.Add(collision.gameObject);
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        for (int i = 0; i < targetTags.Length; i++)
+        {
+            if (collision.gameObject.CompareTag(targetTags[i]))
+            {
+                collisons.Remove(collision.gameObject);
+            }
         }
     }
 }
